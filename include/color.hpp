57bf0c671d6e8b2ce7a665f27e9316b4d61c7af6
MagicNotmac0820 <205513097+MagicNotmac0820@users.noreply.github.com>
2025-06-23 12:03:57 +0800

    Add macros to display color

--- include/color.hpp
+++ include/color.hpp
@@ -11,6 +11,7 @@
 #define BG_GREEN_L "\x1B[48;5;46m"
 #define BG_GRAY "\x1B[48;5;244m"
 #define BG_WHITE "\x1B[48;5;7m"
+#define BG_BLACK "\x1B[48;5;0m"
 #define RED "\x1B[1;38;5;196m"
 #define YELLOW "\x1B[1;38;5;226m"
 #define ORANGE "\x1B[1;38;5;208m"
@@ -21,6 +22,7 @@
 #define GREEN_L "\x1B[1;38;5;46m"
 #define GRAY "\x1B[1;38;5;244m"
 #define WHITE "\x1B[1;38;5;7m"
+#define BLACK "\x1B[1;38;5;0m"
 #define DEFAULT "\x1B[0;m"
 
 #endif  //_COLOR_CPP_H_
